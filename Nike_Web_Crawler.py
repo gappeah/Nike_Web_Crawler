272002642e3e9f06c522a22a290589b02b5f7f71
gappeah <114095068+gappeah@users.noreply.github.com>
2023-10-10 20:53:32 +0100

    Add files via upload

--- Nike_Web_Crawler.py
+++ Nike_Web_Crawler.py
@@ -8,6 +8,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from chromedriver_py import binary_path
 import pandas as pd
+import time
+from datetime import datetime
+import pytz
 
 svc = Service(executable_path=binary_path)
 
@@ -15,7 +18,7 @@ svc = Service(executable_path=binary_path)
 browser = webdriver.Chrome(service=svc)
 
 # Create an empty DataFrame to store the results
-df = pd.DataFrame(columns=["Product", "Name", "Price"])
+df = pd.DataFrame(columns=["Product", "Name", "Price","Time"])
 
 # Define a list of websites to scrape
 websites = [
@@ -35,9 +38,15 @@ for website in websites:
         product_name = WebDriverWait(browser, 20).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div[2]/div/div[4]/div[1]/div/div[2]/div/h1')))
 
-        # If found, extract the text and add to the DataFrame
-        df = df._append({"Product": website, "Name": product_name.get_attribute('innerHTML'), "Price": price.get_attribute('innerHTML')}, ignore_index=True)
-        print("Price for", website, ":","Product Name: " + product_name.get_attribute('innerHTML'), "Price: " + price.get_attribute('innerHTML'))
+        # Get the timezone object for London
+        tz_London = pytz.timezone('Europe/London')
+
+        # Get the current time in London
+        datetime_London = datetime.now(tz_London)
+
+        # If found, extract the text and add time to the DataFrame
+        df = df._append({"Product": website, "Name": product_name.get_attribute('innerHTML'), "Price": price.get_attribute('innerHTML'),"Time": datetime_London.strftime("%H:%M:%S")}, ignore_index=True)
+        print("Price for", website, ":","Product Name: " + product_name.get_attribute('innerHTML'), "Price: " + price.get_attribute('innerHTML'), "London time:", datetime_London.strftime("%H:%M:%S"))
     except:
         # If the element is not found, print an error message
         print("Price or Product was not found for", website)
