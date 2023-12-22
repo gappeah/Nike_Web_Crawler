a47b33e9d2475dd91870c09760e6680636caaf51
George <appeahg@outlook.com>
2023-12-22 23:24:07 +0000

    Remove comments

--- Nike_Web_Crawler.py
+++ Nike_Web_Crawler.py
@@ -1,6 +1,3 @@
-#This project revolves around Python script that takes the price of your favourite trainers from the Nike website and pushes the price into a CSV file
-# The aim of this project is to keep track of the price in case there is a price drop You can customise
-# it by adding time stamps whenever you push your result into the CSV file.
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
